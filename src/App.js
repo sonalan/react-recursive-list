cd102ad8d26ae7bbb893cf876f799efc2275d561
sonalan <sinan@onalan.net>
2022-05-04 23:57:35 -0700

    Imported DataList Item

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import DataList from "./components/DataList";
+
+const data = [
+  { 
+    name: 'Daniel', 
+    age: 40, 
+    children:[
+			{name:'Hunter', age:21, 
+        children:[
+          {name:'Becky', age:2}
+
+			  ]
+      }
+
+	  ]
+  },
+
+  { name: 'John', age: 24 },
+  { 
+    name: 'Jen', 
+    age: 31, 
+		children:[
+			{name:'Baliey', age:5}
+
+	  ]
+  }
+];
 
 function App() {
   return (
     <>
+      <h1>Sample Recersuve Data List App!</h1>
+      <DataList data={data} />
     </>
   );
 }
